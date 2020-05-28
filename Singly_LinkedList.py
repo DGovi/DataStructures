3bbc2ce7a878e2cfe21876622c314bd7b74254f1
Daniel Gauvin <danielgauvin01@gmail.com>
2020-05-27 23:48:04 -0400

    refactoring

--- Singly_LinkedList.py
+++ Singly_LinkedList.py
@@ -1,19 +1,21 @@
 class Node:
 
     def __init__(self, name, data):
-        self.name = name
+        self.name = name.capitalize()
         self.data = data
         self.next = None
 
     def __str__(self):
         # return name of node
-        return "This is node: " + self.name
+        return "Node name:  " + self.name + "\nData: \t\t" + str(self.data) + "\n"
 
 class LinkedList():
 
-    def __init__(self):
-        self.head = None
-        print("LinkedList Created. Names will be automatically capitalized")
+    # if there is a linked list, there is at least one node
+    def __init__(self, first_node_name, first_node_data):
+        self.head = Node(first_node_name, first_node_data)
+        print("LinkedList Create with Node: " + first_node_name +
+              " \n\tNames will be automatically capitalized\n")
 
     def add_at_start(self, node_name, node_data):
         new_node = Node(node_name, node_data)
@@ -22,6 +24,7 @@ class LinkedList():
 
     def add_at_end(self, node_name, node_data):
         current_node = self.head
+
         while current_node.next is not None:
             current_node = current_node.next
         current_node.next = Node(node_name, node_data)
@@ -36,28 +39,40 @@ class LinkedList():
                 current_node.next = new_node
             current_node = current_node.next
 
+    def print_specific_info(self, specified_node_name):
+        current_node = self.head
+
+        while current_node.next is not None:
+            if current_node.name == specified_node_name:
+                print("specified node: \n".upper() +
+                      str(current_node))
+                break
+            current_node = current_node.next
+
     def print_list(self):
         llist = ""
         current_node = self.head
         while current_node.next is not None:
             llist += current_node.name + "->"
             current_node = current_node.next
-        llist += current_node.name + "->x"
-        print(llist)
+        llist += current_node.name + "->Null"
+        print(llist + "\n")
 
-    def print_schedule_information(self):
+    def print_information(self):
         current_node = self.head
+        print("full LList information".upper())
         while current_node.next is not None:
             print(current_node)
             current_node = current_node.next
         print(current_node)
 
 
-schedule = LinkedList()
-schedule.head = Node("a", 5)
+schedule = LinkedList("A", 5)
 schedule.add_at_end("B", 6)
 schedule.add_at_end("c", 7)
 schedule.add_at_start("d", 4)
 schedule.add_after_specified("A", "e", 5)
 
 schedule.print_list()
+schedule.print_specific_info("A")
+schedule.print_information()
